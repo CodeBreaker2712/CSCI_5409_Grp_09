c5621a9d3083774d720d24bc53f673bd9f7efabc
KeneePatel <patelkenee2804@gmail.com>
2024-07-30 16:13:27 -0300

    added Payment Processing

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,18 +1,20 @@
-import { MongoClient, Db } from 'mongodb';
-import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const DB_NAME = "FlexiGym";
-const DB_URI = `${process.env.MONGODB_URI}/${DB_NAME}`
+const DB_URI = `${process.env.MONGODB_URI}/${DB_NAME}`;
 
 const connectDB = async () => {
-    await MongoClient.connect(DB_URI).then(() => {
-        console.log('MongoDB connected');
-    }).catch((error) => {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
+  await MongoClient.connect(DB_URI)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+      process.exit(1);
     });
-}
+};
 
 export default connectDB;
