07e55c564cb6a92b250b067a058f1ea6ef9ab459
alishan <al459703@dal.ca>
2024-07-31 05:13:12 -0300

    Fixed Profile Page Errors

--- frontend/src/Constants/EndPoints.tsx
+++ frontend/src/Constants/EndPoints.tsx
@@ -3,3 +3,4 @@ export const LOGIN_URL = `${BASE_URL}/api/auth/login`;
 export const REGISTRATION_URL = `${BASE_URL}/api/auth/register`;
 export const FORGOT_URL = `${BASE_URL}/api/auth/forgotPassword`;
 export const RESETPW_URL = `${BASE_URL}/api/auth/resetpassword`;
+export const PROFILE_URL = `${BASE_URL}/profile`;
